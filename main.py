a787b530697ad5694af6618b3860b31e7f745573
Felipe Merenda Izidorio <felipemerenda1@gmail.com>
2023-05-30 11:50:17 -0300

    feat: delete Maquina class and mask table; create more atributes for Rede class

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-import pandas as pd
+import math
 from rede import Rede
-from maquina import Maquina
-
-with open('mascaras.csv') as file:
-    all_masks = pd.read_csv(file)
     
 names_net = ['Matriz_DMZ', 'Matriz_Hosts', 'Filial', 'Servidor']
 all_nets = []
+
 for i in range(len(names_net)):
     num_maquinas = int(input(f"Número de máquinas do(a) {names_net[i]}: "))
-    all_nets.append(Rede(name = names_net[i], num_machines = num_maquinas, machines=[Maquina(name = f"maquina{i}") for i in range(num_maquinas)]))
+    all_nets.append(Rede(name = names_net[i], num_machines = num_maquinas))
     
 all_nets.sort(key=lambda x: x.num_machines, reverse=True)
 
 for rede in all_nets:
-    for i, qnt in enumerate(all_masks['Quantidade de hosts']):
-        if rede.num_machines >= qnt:
-            rede.set_mask(all_masks['Máscara'][i])
-
-
-
+    num_0s = round(math.log2(rede.num_maquinas))
+    
+    
     
     
--- rede.py
+++ rede.py
@@ -1,21 +1,28 @@
-from maquina import Maquina
-
 class Rede:
-    def __init__(self, name: str, num_machines: int, ip: str = 'null', mask: str = 'null', machines: list[Maquina] = []):
-        self.name = name
-        self.num_machines = num_machines
-        self.ip = ip
-        self.mask = mask
-        self.machines = machines
+    def __init__(self, nome: str, num_maquinas: int, endereco_rede: str = 'null', endereco_util: str = 'null', endereco_broadcast: str = 'null', mascara: str = 'null'):
+        self.nome = nome
+        self.num_maquinas = num_maquinas
+        self.endereco_rede = endereco_rede
+        self.endereco_util = endereco_util
+        self.endereco_broadcast = endereco_broadcast
+        self.mascara = mascara
         
-    def set_ip(self, ip):
-        self.ip = ip
+    def set_endereco_rede(self, endereco_rede: str):
+        self.endereco_rede = endereco_rede
+        
+    def set_endereco_util(self, endereco_util: str):
+        self.endereco_util = endereco_util
+        
+    def set_endereco_broadcast(self, endereco_broadcast: str):
+        self.endereco_broadcast = endereco_broadcast
     
-    def set_mask(self, mask):
-        self.mask = mask
+    def set_mask(self, mascara: str):
+        self.mascara = mascara
     
     def print_rede(self):
-        print(f'Nome: {self.name}')
-        print(f'Número de máquinas: {self.num_machines}')
-        print(f'IP: {self.ip}')
-        print(f'Máscara: {self.mask}')
+        print(f'Nome: {self.nome}')
+        print(f'Número de máquinas: {self.num_maquinas}')
+        print(f'Endereço Rede: {self.endereco_rede}')
+        print(f'Endereço Util: {self.endereco_util}')
+        print(f'Endereço Broadcast: {self.endereco_broadcast}')
+        print(f'Máscara: {self.mascara}')
